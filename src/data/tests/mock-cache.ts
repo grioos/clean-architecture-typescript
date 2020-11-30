ad5247bb5220f1d3717a75afa35011f77eb91106
grios <gabriel.al.rio@gmail.com>
2020-11-30 10:42:42 -0300

    ensure LocalLoadPurchases returns empty list on load failure

--- src/data/tests/mock-cache.ts
+++ src/data/tests/mock-cache.ts
@@ -42,6 +42,13 @@ export class CacheStoreSpy implements CacheStore {
       throw new Error()
     })
   }
+
+  simulateFecthError(): void {
+    jest.spyOn(CacheStoreSpy.prototype, 'fetch').mockImplementationOnce(() => {
+      this.actions.push(CacheStoreSpy.Action.fetch)
+      throw new Error()
+    })
+  }
 }
 
 export namespace CacheStoreSpy {
