eb22e3b6cfa970d0749659ac40274f7117739867
grios <gabriel.al.rio@gmail.com>
2020-11-30 12:45:35 -0300

    ensure LocalLoadPurchases deletes old cache if fails

--- src/data/usecases/load-purchases/local-validate-purchases.spec.ts
+++ src/data/usecases/load-purchases/local-validate-purchases.spec.ts
@@ -22,4 +22,14 @@ describe('LocalLoadPurchases', () => {
 
     expect(cacheStore.actions).toEqual([])
   })
+
+  test('Should delete cache if load fails', async () => {
+    const { cacheStore, sut } = makeSut()
+
+    cacheStore.simulateFecthError()
+    sut.validate()
+
+    expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.fetch, CacheStoreSpy.Action.delete])
+    expect(cacheStore.deleteKey).toBe('purchases')
+  })
 })
